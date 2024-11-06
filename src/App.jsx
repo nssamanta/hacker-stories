2a44baabbcab38116235adad11cfe672e8c3f5e6
nssamanta <144923305+nssamanta@users.noreply.github.com>
2024-11-05 22:26:30 -0800

    Initial Commit Project Installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -32,4 +32,67 @@ function App() {
   )
 }
 
-export default App
+export default App*/
+
+import * as React from "react";
+
+//const title = "React";
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const welcome = {
+  title: "React",
+  greeting: "Hey",
+};
+
+const candies = ["Chocolate", "Gummies", "Candy Canes", "Tamarindo"];
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+       <hr />
+
+      <ul>
+        {candies.map((candy, index) => (
+          <li key={index}>{candy} </li>
+        ))}
+      </ul>
+
+      <ul>
+        {list.map(function (item) {return <li key={item.objectID}>
+          <span><a href={item.url}>{item.title}</a></span>
+          <span> {item.author}</span>
+          <span> {item.num_comments}</span>
+          <span> {item.points}</span>
+        </li>})}
+      </ul>
+    </div>
+  );
+}
+export default App;
