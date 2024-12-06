7c7adbed39816f7d5d54bb473d2126a3f6e604e3
nssamanta <144923305+nssamanta@users.noreply.github.com>
2024-12-06 14:59:03 -0800

    callback handler functions

--- src/App.jsx
+++ src/App.jsx
@@ -42,76 +42,100 @@ const welcome = {
   greeting: "Hey",
 };
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const candies = ["Chocolate", "Gummies", "Candy Canes", "Tamarindo"];
 
-const App = () => (
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  //A: callback handler gets introduced as event handler
+  const handleSearch = (event) => {
+    //D: callback to the place it was introduced
+    console.log(event.target.value);
+  };
+  return (
     <div>
       <h1>
         {welcome.greeting} {welcome.title}
       </h1>
-      <Search />
+      {/* // B: passed as function in props to another component */}
+      <Search onSearch={handleSearch} />
       <hr />
-      <List />
-     
+      <List list={stories} />
+
       <hr />
       <Candies />
     </div>
-);
+  );
+};
 
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  /*let searchTerm = "";*/
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+   /* searchTerm = event.target.value;*/
+    // synthetic event letter typed in
+   /* console.log(event);*/
+    //C:Is executed there as callback handler
+    props.onSearch(event);
+    // value of target (here: input HTML element)
+    /*console.log(event.target.value);*/
+  };
 
-
-const Search = () => (
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
+  );
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
 );
 
-
-const List = () => (
-    <ul>
-      {list.map((item) => {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span> {item.author}</span>
-            <span> {item.num_comments}</span>
-            <span> {item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}> {props.item.title}</a>
+    </span>
+    <span> {props.item.author}</span>
+    <span> {props.item.num_comments}</span>
+    <span> {props.item.points}</span>
+  </li>
 );
 
 const Candies = () => (
-    <div>
-        <ul>
-        {candies.map((candy, index) => (
-          <li key={index}>{candy} </li>
-        ))}
-      </ul>
-    </div>
+  <div>
+    <ul>
+      {candies.map((candy, index) => (
+        <li key={index}>{candy} </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default App;
